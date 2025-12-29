902c70803246c9d843e4f65b637cc59870d7c473
smithrowe-10 <smj0418smj@gmail.com>
2025-12-29 17:50:38 +0900

    20251229

--- src/components/openai/OpenaiApiModal.jsx
+++ src/components/openai/OpenaiApiModal.jsx
@@ -1,18 +1,60 @@
 import { sentTextOpenai } from "../../apis/openai/openaiApi";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+
+function TypingEffect({text, speed = 50}) {
+    const [ displayText, setDisplayText ] = useState("");
+    const [ isTyping, setTyping ] = useState(true);
+    const textIndex = useRef(0);
+
+    useEffect(() => {
+        setDisplayText("");
+        setTyping(true);
+
+        const timer = setInterval(() => {
+            if (textIndex.current < text.length) {
+                setDisplayText(displayText + text[textIndex.current]);
+                textIndex.current += 1;
+            } else {
+                setTyping(false);
+                clearInterval(timer);
+            }
+        }, speed);
+
+        return () => clearInterval(timer);
+    }, [text]);
+    
+    return <div>
+        {displayText}
+        {isTyping && <span>|</span>}
+    </div>
+}
 
 function OpenaiApiModal() {
 
     const [ inputValue, setInputValue ] = useState("");
+    const [ response, setResponse ] = useState(); 
+    const [ isLoading, setLoading ] = useState(false);
 
     const handleSend = async () => {
+        setLoading(true);
         const response = await sentTextOpenai(inputValue);
-        console.log(response.output_text);
+        setResponse(response);
     }
+
+    useEffect(() => {
+        if (isLoading) {
+            setLoading(false);
+        }
+    }, [response]);
 
     return <div>
         <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
         <button onClick={handleSend}>전송</button>
+        {
+            !isLoading && !!response &&
+            <TypingEffect text={response.output_text}/>
+        }
     </div>
 }
 
